59d8e381e394eb75a807b88da2f68d750a0da274
Cliff Zhang <cliffzhang08@gmail.com>
2022-09-20 20:59:19 -0400

    client: Remove react-box things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,10 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
-import "./App.css";
 
 function App() {
   return (
     <EthProvider>
       <div id="App" >
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+        <h1>unleashed</h1>
       </div>
     </EthProvider>
   );
